c28637061745fd21c42618505f573bf53ecaf1fe
Karan Sheth <karan.sheth60@gmail.com>
2020-12-03 11:25:23 -0500

    Removed extea Env variable

--- src/components/Stats/AllStatsController.js
+++ src/components/Stats/AllStatsController.js
@@ -1,19 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import StatsView from './StatsView';
 import { fetchDataForStats } from './StatsState';
 
+// eslint-disable-next-line arrow-body-style
 const Stats = () => {
-  const data = useSelector((state) => {
-    if (!state.stats.isFetched) {
-      const dispatch = useDispatch();
-      dispatch(fetchDataForStats());
-    }
-    return state.stats.data;
-  });
+  // const data = useSelector((state) => {
+  //   if (!state.stats.isFetched) {
+  //     const dispatch = useDispatch();
+  //     dispatch(fetchDataForStats());
+  //   }
+  //   return state.stats.data;
+  // });
 
-  return (!data || data.length === 0 ? (<CircularProgress />) : <StatsView data={data} />);
+  // return (!data || data.length === 0 ? (<CircularProgress />) : <StatsView data={data} />);
+  return '';
 };
 
 export default (Stats);
