847efb4b0b257a55e782ceb2e417ca06f56735c1
jonkiky <jonkiky@gmail.com>
2021-01-04 15:41:43 -0500

    add program detail page

--- src/bento/programData.js
+++ src/bento/programData.js
@@ -8,7 +8,7 @@ const pageData = {
     prgramName: 'COP',
     primaryImage: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/programCards/cop/copPrimary.png',
     primaryImageAlt: 'The Center for Cancer Research (CCR) is the largest division of the NCI intramural research program and comprises nearly 250 basic and clinical research groups located on two campuses outside of Washington, DC.',
-    secondaryImage: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/programCards/cop/copSecondary.png',
+    secondaryImage: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/programCards/cop/copSecondary.jpg',
   },
 };
 
