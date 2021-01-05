583b3c87c07ced68f6c043d7947778fedd28761e
skiran86 <skphp9@gmail.com>
2021-01-05 15:22:42 -0500

    statsBar init

--- src/bento/globalStatsData.js
+++ src/bento/globalStatsData.js
@@ -2,54 +2,58 @@ import gql from 'graphql-tag';
 
 export const statsStyling = {
   global: {
-    horizontalStyle: true,
-    statTitleFirst: true,
-    height: '47px',
-    background: '#8DCAFF',
+    horizontalStyle: false,
+    statTitleFirst: false,
+    height: '62px',
+    background: '#CBE2EE',
   },
 };
 
 export const globalStatsData = [
   // A maximum of 6 stats are allowed
   {
-    statTitle: 'Programs',
-    type: 'field',
-    statAPI: 'numberOfPrograms',
-  },
-  {
-    statTitle: 'Arms',
+    statTitle: 'Studies',
     type: 'field',
     statAPI: 'numberOfStudies',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/icons/Icon-studies-stats.svg',
+    statIconAlt: 'Studies Stats Bar Icon',
   },
   {
     statTitle: 'Cases',
     type: 'field',
-    statAPI: 'numberOfSubjects',
+    statAPI: 'numberOfCases',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/icons/Icon-cases-stats.svg',
+    statIconAlt: 'Studies Stats Bar Icon',
   },
   {
-    statTitle: 'samples',
+    statTitle: 'Samples',
     type: 'field',
     statAPI: 'numberOfSamples',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/icons/Icon-samples-stats.svg',
+    statIconAlt: 'Samples Stats Bar Icon',
   },
   {
-    statTitle: 'Assays',
-    type: 'field',
-    statAPI: 'numberOfLabProcedures',
-  },
-  {
-    statTitle: 'files',
+    statTitle: 'Files',
     type: 'field',
     statAPI: 'numberOfFiles',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/icons/Icon-files-stats.svg',
+    statIconAlt: 'Files Stats Bar Icon',
+  },
+  {
+    statTitle: 'Aliquots',
+    type: 'field',
+    statAPI: 'numberOfAliquots',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/icons/Icon-aliquots-stats.svg',
+    statIconAlt: 'Aliquots Stats Bar Icon',
   },
 ];
 
 // --------------- GraphQL query - Retrieve stats details --------------
 export const GET_GLOBAL_STATS_DATA_QUERY = gql`{
-  numberOfPrograms
   numberOfStudies
-  numberOfSubjects
+  numberOfCases
   numberOfSamples
-  numberOfLabProcedures
   numberOfFiles
+  numberOfAliquots
   }
   `;
