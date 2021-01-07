6576765081f7a9790f4a4ca7f3c2363d4bf37629
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2021-01-07 11:11:41 -0500

    undo linkbar

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withStyles, CssBaseline } from '@material-ui/core';
-import LinkBar from 'bento-components';
+// import LinkBar from 'bento-components';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Header from '../Header/HeaderView';
 import NavBar from '../NavBar/NavBarContainer';
@@ -21,7 +21,7 @@ const Layout = ({ classes, isSidebarOpened }) => (
     <CssBaseline />
     <HashRouter>
       <>
-        <LinkBar />
+        {/* <LinkBar /> */}
         <Header />
         <NavBar />
         {/* Reminder: Ajay need to replace the ICDC with env variable and
