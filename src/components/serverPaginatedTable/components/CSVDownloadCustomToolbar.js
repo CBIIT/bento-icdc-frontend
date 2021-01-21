2a76723aa26cc10ee7fb8a8fca5dc7061cf6efab
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2021-01-21 14:43:15 -0500

    lint-fixes

--- src/components/serverPaginatedTable/components/CSVDownloadCustomToolbar.js
+++ src/components/serverPaginatedTable/components/CSVDownloadCustomToolbar.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -7,24 +6,21 @@ import { withStyles } from '@material-ui/core/styles';
 import client from '../../../utils/graphqlClient';
 import { downloadJson } from './utils';
 
-
-
 const defaultToolbarStyles = {
   iconButton: {
   },
 };
 
 const CustomToolbar = ({
-  classes,tableDownloadCSV, queryCustomVaribles
+  classes, tableDownloadCSV, queryCustomVaribles,
 }) => {
-
   async function fetchData() {
     const fetchResult = await client
       .query({
         query: tableDownloadCSV.query,
         variables: {
           first: 1000,
-          ...queryCustomVaribles
+          ...queryCustomVaribles,
         },
       })
       .then((result) => result.data[tableDownloadCSV.apiVariable]);
@@ -39,17 +35,18 @@ const CustomToolbar = ({
     );
   }
 
-  
   return (
     <>
       <Tooltip title="Download Full Table As CSV">
-        <IconButton className={classes.iconButton} onClick={()=>prepareDownload(tableDownloadCSV)}>
+        <IconButton
+          className={classes.iconButton}
+          onClick={() => prepareDownload(tableDownloadCSV)}
+        >
           <DownloadIcon className={classes.downloadIcon} />
         </IconButton>
       </Tooltip>
     </>
   );
-
-}
+};
 
 export default withStyles(defaultToolbarStyles, { name: 'CustomToolbar' })(CustomToolbar);
