7e3ed42a7951e832577e957fc0032fe84de83935
ambar <ambar.rana123@gmail.com>
2021-04-29 09:25:21 -0400

    header img url update
    img updated to 3000px

--- src/bento/globalHeaderData.js
+++ src/bento/globalHeaderData.js
@@ -2,5 +2,5 @@ export default {
   globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/header/icdc_nih_logo.svg',
   globalHeaderLogoLink: '/',
   globalHeaderLogoAltText: 'ICDC Logo',
-  globalHeaderImage: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/header/Canine2000.png',
+  globalHeaderImage: 'https://raw.githubusercontent.com/CBIIT/bento-icdc-frontend/master/src/assets/header/Canine3000.png',
 };
