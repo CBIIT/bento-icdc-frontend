934b084bcb17b36f9c401dc0fd8156939704db14
[Toyo Udosen] <[tyudosen@gmail.com]>
2021-08-03 08:28:50 -0500

    caseDetail view D-QA

--- src/pages/caseDetails/components/multiStudyCases.js
+++ src/pages/caseDetails/components/multiStudyCases.js
@@ -61,7 +61,8 @@ const StyledBadge = withStyles((theme) => ({
     fontSize: '8px',
     fontFamily: 'Open Sans',
     backgroundColor: '#FFF',
-    fontWeight: '600',
+    fontWeight: '700',
+    color: '#000000',
   },
 }))(Badge);
 
