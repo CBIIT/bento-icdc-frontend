f2b42c2c702e667357f018a30d59fafd509395f8
[Toyo Udosen] <[tyudosen@gmail.com]>
2021-11-12 07:41:53 -0600

    test

--- src/components/ProgramCard/__test__/programCard.test.js
+++ src/components/ProgramCard/__test__/programCard.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import ProgramCard from '../programCard';
