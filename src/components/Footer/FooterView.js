19e1289a76e1734b069b629fea9f447169c05da9
rana22 <ambar.rana123@gmail.com>
2022-01-11 16:33:40 -0500

    icdc_2114 display FS version on Footer

--- src/components/Footer/FooterView.js
+++ src/components/Footer/FooterView.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Footer } from 'bento-components';
 import FooterData from '../../bento/globalFooterData';
+import env from '../../utils/env';
 
-const ICDCFooter = () => <><Footer data={FooterData} background={FooterData.bg} /></>;
+const FILE_SERVICE_API = env.REACT_APP_FILE_SERVICE_API;
+
+const ICDCFooter = () => {
+  const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
+
+  useEffect(() => {
+    const getSystems = async () => {
+      const response = await axios.get(FILE_SERVICE_API.concat('version'));
+      try {
+        const { data } = response;
+        const FSverison = { FileServiceVersion: data.version || '' };
+        setFooterUpdatedData({ ...FooterData, ...FSverison });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getSystems();
+  }, [FooterData]);
+  return <><Footer data={footerUpdatedData} /></>;
+};
 
 export default ICDCFooter;
