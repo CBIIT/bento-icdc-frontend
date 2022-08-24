731515488921bdfc2a2ed8abb9b63b3ccd0ad354
[Toyo Udosen] <[tyudosen@gmail.com]>
2022-08-24 09:31:19 -0500

    DAL sort fix

--- src/pages/studies/studiesController.js
+++ src/pages/studies/studiesController.js
@@ -5,11 +5,25 @@ import Studies from './studiesView';
 import { Typography } from '../../components/Wrappers/Wrappers';
 import { GET_STUDY_DATA_QUERY } from '../../bento/studiesData';
 
+const convertCRDCLinksToValue = (data) => {
+  const objString = Object.entries(data)[0][0];
+  const dataArr = Object.entries(data)[0][1];
+  const processedArr = dataArr
+    .map((element) => ({
+      ...element, CRDCLinks: element.CRDCLinks.length, links: element.CRDCLinks,
+    }));
+  const tempArr = [
+    [objString, processedArr],
+  ];
+  return Object.fromEntries(tempArr);
+};
+
 const studiesContainer = ({ invalid }) => {
   const { loading, error, data } = useQuery(GET_STUDY_DATA_QUERY);
   if (loading) return <CircularProgress />;
   if (error) return <Typography variant="headline" color="error" size="sm">{error ? `An error has occurred in loading stats component: ${error}` : 'Recieved wrong data'}</Typography>;
-  return <Studies data={data} invalid={invalid} />;
+
+  return <Studies data={convertCRDCLinksToValue(data)} invalid={invalid} />;
 };
 
 export default studiesContainer;
