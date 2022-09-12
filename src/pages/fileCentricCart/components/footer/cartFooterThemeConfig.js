10ac064c60661ba5fb2cb815299c1a1727438ab9
rana22 <ambar.rana123@gmail.com>
2022-09-12 15:28:59 -0400

    jbrowse btn style

--- src/pages/fileCentricCart/components/footer/cartFooterThemeConfig.js
+++ src/pages/fileCentricCart/components/footer/cartFooterThemeConfig.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import themes, { overrides } from '../../../../themes';
 
 export default ({
   children,
 }) => {
-  const theme = {
-    overrides: {
-      MuiButton: {
-        root: {
-          height: ' 36px',
-          color: '#ffffff',
-          textTransform: 'inherite',
-          borderRadius: '2px',
-        },
-        '&.disabled': {
-          color: '#ffffff',
-        },
-      },
-      '&.MuiButton-disabled': {
+  const style = [];
+  const overridesObj = themes.light.overrides;
+  const MuiButton = {
+    root: {
+      textTransform: 'inherite',
+      '&#jbrowse_multi_view_button': {
+        height: ' 36px',
         color: '#ffffff',
-      },
-      MuiIconButton: {
-        root: {
-        },
+        borderRadius: '5px',
       },
     },
   };
 
+  overridesObj.MuiButton = MuiButton;
+
+  style.push(overridesObj);
+  const computedTheme = createTheme({ ...themes.light, ...overrides, ...style });
+
   return (
-    <MuiThemeProvider theme={createTheme(theme)}>
+    <MuiThemeProvider theme={computedTheme}>
       {children}
     </MuiThemeProvider>
   );
