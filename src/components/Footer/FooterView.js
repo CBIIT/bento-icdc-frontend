ad35a830414f1e8991706583dafaab884831371e
rana22 <ambar.rana123@gmail.com>
2022-10-31 10:24:44 -0400

    set new footer style

--- src/components/Footer/FooterView.js
+++ src/components/Footer/FooterView.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Footer } from 'bento-components';
 import FooterData from '../../bento/globalFooterData';
 import env from '../../utils/env';
+import CustomThemeProvider from './FooterThemConfig';
 
 const FILE_SERVICE_API = env.REACT_APP_FILE_SERVICE_API;
 
@@ -14,8 +15,10 @@ const ICDCFooter = () => {
       const response = await axios.get(`${FILE_SERVICE_API}version`);
       try {
         const { data } = response;
-        const FSverison = { FileServiceVersion: data.version || '' };
-        setFooterUpdatedData({ ...FooterData, ...FSverison });
+        // const FSverison = { FileServiceVersion: data.version || '' };
+        const SystemInfo = FooterData.link_sections[2];
+        SystemInfo.items.push({ text: `FS Version: ${data.version}` });
+        setFooterUpdatedData({ ...FooterData, ...SystemInfo });
       } catch (error) {
         const FSverison = { FileServiceVersion: 'Error in getting File service verison' };
         setFooterUpdatedData({ ...FooterData, ...FSverison });
@@ -23,7 +26,11 @@ const ICDCFooter = () => {
     };
     getSystems();
   }, [FooterData]);
-  return <><Footer data={footerUpdatedData} /></>;
+  return (
+    <CustomThemeProvider>
+      <Footer data={footerUpdatedData} />
+    </CustomThemeProvider>
+  );
 };
 
 export default ICDCFooter;
