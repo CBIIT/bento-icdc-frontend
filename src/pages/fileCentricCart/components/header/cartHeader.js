0c43549a602d42b9b515a402f90b87516542005d
Toyo <udosent2@nih.gov>
2023-07-03 13:09:17 -0500

    storeManifest query

--- src/pages/fileCentricCart/components/header/cartHeader.js
+++ src/pages/fileCentricCart/components/header/cartHeader.js
@@ -83,7 +83,7 @@ const getReadMe = async (setContent, url) => {
 };
 
 const STORE_MANIFEST_QUERY = gql`
-    query storeManifest($manifest: [FileInList!]!) {
+    query storeManifest($manifest: String!) {
         storeManifest(manifest: $manifest)
     }
 `;
@@ -98,65 +98,11 @@ const CartHeader = React.forwardRef(({
   manifestPayload,
 }, ref) => {
   console.log('log maniPay', { manifestPayload, type: typeof manifestPayload, string: JSON.stringify(manifestPayload) });
-  // const STORE_MANIFEST_QUERY = gql`{
-  //   storeManifest(manifest: [${manifestPayload ? manifestPayload.map((obj) => {
-  //   const entries = Object.entries(obj);
-  //   const formattedEntries = entries.map(([key, value]) => `${key}: ${JSON.stringify(value)}`);
-  //   return `{${formattedEntries.join(', ')}}`;
-  // }).join(',') : [{}]}])
-  // }
-  // `;
-
-  // const STORE_MANIFEST_QUERY = gql`{
-  //   storeManifest(manifest: [{
-  //     file_name: "010015_0103_sorted.bam",
-  //     file_type: "RNA Sequence File",
-  //     association: "sample",
-  //     file_description: "tumor sample binary alignment file: seq reads to canfam3.1",
-  //     file_format: "bam",
-  //     file_size: 17545870661,
-  //     case_id: "NCATS-COP01-CCB010015",
-  //     breed: "Mixed Breed",
-  //     diagnosis: "B Cell Lymphoma",
-  //     study_code: "NCATS-COP01",
-  //     file_uuid: "bf7ae08f-0afe-5aa5-969a-de9a17ac0f2f",
-  //     md5sum: "70ec6bee3d4e5bb9da4641c3fa7f8609",
-  //     sample_id: "NCATS-COP01-CCB010015 0103",
-  //     individual_id: null,
-  //     name: "010015_0103_sorted.bam",
-  //     drs_uri: "https://nci-crdc.datacommons.io/ga4gh/drs/v1/objects/dg.4DFC/bf7ae08f-0afe-5aa5-969a-de9a17ac0f2f",
-  //   }, {
-  //     file_name: "010015_0103_sorted.bam",
-  //     file_type: "RNA Sequence File",
-  //     association: "sample",
-  //     file_description: "tumor sample binary alignment file: seq reads to canfam3.1",
-  //     file_format: "bam",
-  //     file_size: 17545870661,
-  //     case_id: "NCATS-COP01-CCB010015",
-  //     breed: "Mixed Breed",
-  //     diagnosis: "B Cell Lymphoma",
-  //     study_code: "NCATS-COP01",
-  //     file_uuid: "bf7ae08f-0afe-5aa5-969a-de9a17ac0f2f",
-  //     md5sum: "70ec6bee3d4e5bb9da4641c3fa7f8609",
-  //     sample_id: "NCATS-COP01-CCB010015 0103",
-  //     individual_id: null,
-  //     name: "010015_0103_sorted.bam",
-  //     drs_uri: "https://nci-crdc.datacommons.io/ga4gh/drs/v1/objects/dg.4DFC/bf7ae08f-0afe-5aa5-969a-de9a17ac0f2f",
-  //   }])
-  // }
-  // `;
-  // const { data } = useQuery({
-  //   queryKey: ['storeManifest'],
-  //   queryFn: async () => request(
-  //     env.REACT_APP_INTEROP_SERVICE_URL,
-  //     STORE_MANIFEST_QUERY,
-  //   ),
-  //   enabled: !!manifestPayload,
-  // });
 
   const { data } = useQuery(STORE_MANIFEST_QUERY, {
-    variables: { manifest: manifestPayload },
+    variables: { manifest: JSON.stringify(manifestPayload) },
     context: { clientName: 'interopService' },
+    skip: !manifestPayload,
     fetchPolicy: 'no-cache',
   });
 
