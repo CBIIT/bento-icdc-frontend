bc626d66fff60127dc7a600730ffafc705dab502
rana22 <ambar.rana123@gmail.com>
2023-10-17 13:25:59 -0400

    revert code

--- src/components/NavBar/NavBarView.js
+++ src/components/NavBar/NavBarView.js
@@ -3,16 +3,15 @@ import { NavBar } from '@bento-core/nav-bar';
 import {
   navBarData, navBarCartData, navBarstyling,
 } from '../../bento/navigationBarData';
-import NavThemeProvider from './NavTheme';
 
 const BentoNavBar = ({ cartFieldIds = [] }) => (
-  <NavThemeProvider>
+  <>
     <NavBar
       navBarData={navBarData}
       navBarCartData={navBarCartData}
       navBarstyling={navBarstyling}
       numberOfCases={cartFieldIds.length}
     />
-  </NavThemeProvider>
+  </>
 );
 export default BentoNavBar;
