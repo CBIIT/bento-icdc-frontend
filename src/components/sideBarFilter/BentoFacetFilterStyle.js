a972f97b64ad619ab0484361a479c3d8c459f9f6
rana22 <ambar.rana123@gmail.com>
2023-10-20 13:28:28 -0400

    fix filter bar style

--- src/components/sideBarFilter/BentoFacetFilterStyle.js
+++ src/components/sideBarFilter/BentoFacetFilterStyle.js
@@ -1,21 +1,4 @@
 export default () => ({
-  resetButton: {
-    fontSize: '9px',
-    marginTop: '4px',
-    minHeight: '25px',
-    fontFamily: 'Lato, Open Sans, sans-serif',
-    marginLeft: '16px',
-    borderRadius: '100px',
-    textTransform: 'none',
-    '&:hover': {
-      backgroundColor: '#566672',
-      color: '#fff',
-    },
-  },
-  floatRight: {
-    margin: '7px 0px 15px 6px',
-    paddingTop: '10px',
-  },
   restBtn: {
     margin: '7px 0px 15px 6px',
     paddingTop: '10px',
@@ -24,36 +7,12 @@ export default () => ({
   dropDownIconSection: {
     color: '#000000',
   },
-  sectionSummaryText: {
-    marginLeft: '10px',
-    lineHeight: 0,
-    color: '#323232',
-    fontFamily: 'Raleway',
-    fontSize: '13px',
-    fontWeight: 'bold',
-    letterSpacing: '0.25px',
-  },
   dropDownIconSubSection: {
     marginLeft: '0px',
     fill: '#000000',
   },
-  customExpansionPanelSummaryRoot: {
-    flexDirection: 'row-reverse',
-    paddingLeft: 0,
-  },
   disableExpansion: {
     backgroundColor: 'rgba(0, 0, 0 , 0.17)',
-  },
-  sectionSummaryTextContainer: {
-    marginLeft: '10px',
-    color: '#323232',
-    fontFamily: 'Raleway',
-    fontSize: '15px',
-    fontWeight: 'bold',
-    letterSpacing: '0.25px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
   },
   activeFacetFilterByCases: {
     color: '#ff7f15',
@@ -64,56 +23,6 @@ export default () => ({
   activeFacetFilterByFiles: {
     color: '#667a87',
   },
-  searchContainer: {
-    paddingTop: '15px',
-    margin: '0 2px',
-    marginRight: 6,
-  },
-  findCaseButton: {
-    backgroundColor: '#FCA359',
-    boxSizing: 'border-box',
-    height: 30,
-    width: 40,
-    border: '1.25px solid #f2ab71',
-    cursor: 'pointer',
-    borderRadius: 11,
-    display: 'flex',
-    marginRight: 6,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  findCaseIcon: {
-    width: 17,
-    height: 17,
-  },
-  uploadButton: {
-    boxSizing: 'border-box',
-    fontWeight: '400',
-    height: 32,
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    backgroundColor: '#FCA359',
-    color: '#fff',
-    border: '1px solid #f2ab71',
-    borderRadius: 10,
-    fontFamily: 'Lato',
-    fontSize: 11,
-    boxShadow: 'none',
-    paddingLeft: 16,
-    paddingRight: 12,
-    '&:hover': {
-      backgroundColor: '#FCA359',
-    },
-  },
-  iconSpan: {
-    marginTop: '5.2px',
-  },
-  uploadIcon: {
-    height: 19,
-    width: 19,
-  },
-
 });
 
 export const customStyles = {
