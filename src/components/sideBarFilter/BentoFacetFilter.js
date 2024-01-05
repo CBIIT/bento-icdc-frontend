6249100f35f1efba3cf91a6eae06bdeb1489efb9
Udosen <udosent2@nih.gov>
2024-01-05 12:38:00 -0600

    clear facet by section

--- src/components/sideBarFilter/BentoFacetFilter.js
+++ src/components/sideBarFilter/BentoFacetFilter.js
@@ -1,25 +1,36 @@
-import React, { useCallback, useMemo, useState } from 'react';
+/* eslint-disable */
+import React, { useCallback, useMemo, useState } from "react";
 import {
   AccordionSummary,
   Button,
   Container,
   withStyles,
-} from '@material-ui/core';
+} from "@material-ui/core";
 import {
   // ArrowDropDown as ArrowDropDownIcon,
   ExpandMore as ExpandMoreIcon,
-} from '@material-ui/icons';
-import clsx from 'clsx';
+} from "@material-ui/icons";
+import clsx from "clsx";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import IconButton from "@material-ui/core/IconButton";
 import {
-  ClearAllFiltersBtn, FacetFilter, resetAllData, chunkSplit,
-  SearchView, SearchBoxGenerator, UploadModalGenerator,
-} from '../../bento-core';
+  ClearAllFiltersBtn,
+  FacetFilter,
+  resetAllData,
+  chunkSplit,
+  SearchView,
+  SearchBoxGenerator,
+  UploadModalGenerator,
+} from "../../bento-core";
 
-import FacetFilterThemeProvider from './FilterThemeConfig';
-import styles, { customStyles, uploadCustomStyles } from './BentoFacetFilterStyle';
-import { getAllIds, getAllSubjectIds } from './BentoFilterUtils';
-import store from '../../store';
-import { localFindConfig } from '../../bento/localSearchData';
+import FacetFilterThemeProvider from "./FilterThemeConfig";
+import styles, {
+  customStyles,
+  uploadCustomStyles,
+} from "./BentoFacetFilterStyle";
+import { getAllIds, getAllSubjectIds } from "./BentoFilterUtils";
+import store from "../../store";
+import { localFindConfig } from "../../bento/localSearchData";
 
 const CustomExpansionPanelSummary = withStyles({
   root: {
@@ -28,14 +39,14 @@ const CustomExpansionPanelSummary = withStyles({
     paddingLeft: 14,
     paddingRight: 14,
     minHeight: 48,
-    '&$expanded': {
+    "&$expanded": {
       minHeight: 48,
     },
   },
   content: {
-    display: 'block',
-    '&$expanded': {
-      margin: '4px 0px 15px 0px',
+    display: "block",
+    "&$expanded": {
+      margin: "4px 0px 15px 0px",
     },
   },
   expanded: {},
@@ -57,9 +68,9 @@ const { SearchBox } = SearchBoxGenerator({
     },
   },
   config: {
-    inputPlaceholder: 'e.g. ICDC-CASE-06, ICDC-CASE-22',
-    noOptionsText: 'No matching items found',
-    searchType: 'case_id',
+    inputPlaceholder: "e.g. ICDC-CASE-06, ICDC-CASE-22",
+    noOptionsText: "No matching items found",
+    searchType: "case_id",
   },
   customStyles,
 });
@@ -71,9 +82,12 @@ const { UploadModal } = UploadModalGenerator({
       try {
         // Split the search terms into chunks of 500
         const caseChunks = chunkSplit(inputArray, 500);
-        const matched = (await Promise.allSettled(caseChunks.map((chunk) => (
-          getAllSubjectIds(chunk)))))
-          .filter((result) => result.status === 'fulfilled')
+        const matched = (
+          await Promise.allSettled(
+            caseChunks.map((chunk) => getAllSubjectIds(chunk))
+          )
+        )
+          .filter((result) => result.status === "fulfilled")
           .map((result) => result.value || [])
           .flat(1);
 
@@ -102,20 +116,31 @@ const BentoFacetFilter = ({
 }) => {
   // set tooltip text progams / biobank
   // useMemo to prevent execution of func everytime component re renders
-  const tooltipText = useMemo(() => tooltipItems.reduce((acc, item) => {
-    const { __typename: datafield } = item;
-    const { acronym, name } = tooltipConfig[datafield];
-    return { ...acc, [datafield]: { ...acc[datafield], [item[acronym]]: item[name] } };
-  }, {}), []);
+  const tooltipText = useMemo(
+    () =>
+      tooltipItems.reduce((acc, item) => {
+        const { __typename: datafield } = item;
+        const { acronym, name } = tooltipConfig[datafield];
+        return {
+          ...acc,
+          [datafield]: { ...acc[datafield], [item[acronym]]: item[name] },
+        };
+      }, {}),
+    []
+  );
 
   /**
    * Add Bento frontend filter count/subjects
    * Add tootip text
    */
-  const updateFacetConfig = useMemo(() => facetsConfig.map((item) => ({
-    ...item,
-    customCount: (text) => `${text || 0}`,
-  })), []);
+  const updateFacetConfig = useMemo(
+    () =>
+      facetsConfig.map((item) => ({
+        ...item,
+        customCount: (text) => `${text || 0}`,
+      })),
+    []
+  );
 
   const filterData = facetsConfig.reduce((acc, item) => {
     const facetValues = searchData[item.apiPath];
@@ -139,141 +164,165 @@ const BentoFacetFilter = ({
   }, {});
 
   /**
-  * Clear All Filter Button
-  * Custom button component
-  * bento core params
-  * 1. onClearAllFilters - dispatch clear all filters
-  * 2. disable - true/ false
-  */
-  const CustomClearAllFiltersBtn = useCallback(({ onClearAllFilters, disable }) => (
-    <Container className="floatRight">
-      <Button
-        variant="outlined"
-        disabled={disable}
-        className="resetButton"
-        onClick={() => {
-          onClearAllFilters();
-          store.dispatch(resetAllData());
-        }}
-        disableRipple
-      >
-        CLEAR ALL
-      </Button>
-    </Container>
-  ), []);
-
-  /** Note:
-  * Generate Custom facet Section Component
-  * 1. Config local search input for Case
-  * 2. Facet Section Name
-  */
-  const CustomFacetSection = useCallback(({ section }) => {
-    const { name, expandSection } = section;
-    const { hasSearch = false } = facetSectionVariables[name];
-    const [expanded, setExpanded] = useState(expandSection);
-    const [showSearch, setShowSearch] = useState(true);
-
-    const toggleSearch = (e) => {
-      e.stopPropagation();
-      setShowSearch(!showSearch);
-    };
-    const collapseHandler = () => setExpanded(!expanded);
-
-    return (
-      <>
-        <CustomExpansionPanelSummary
-          onClick={collapseHandler}
-          id={section}
-          className={clsx(
-            {
-              [classes.disableExpansion]: isUnifiedView,
-            },
-            'customExpansionPanelSummaryRoot',
-          )}
-          disabled={isUnifiedView}
+   * Clear All Filter Button
+   * Custom button component
+   * bento core params
+   * 1. onClearAllFilters - dispatch clear all filters
+   * 2. disable - true/ false
+   */
+  const CustomClearAllFiltersBtn = useCallback(
+    ({ onClearAllFilters, disable }) => (
+      <Container className="floatRight">
+        <Button
+          variant="outlined"
+          disabled={disable}
+          className="resetButton"
+          onClick={() => {
+            onClearAllFilters();
+            store.dispatch(resetAllData());
+          }}
+          disableRipple
         >
-          <div className="sectionSummaryTextContainer">
-            {name}
-            {hasSearch && (
-              <div className="findCaseButton" onClick={toggleSearch}>
-                <img
-                  src="https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/svgs/FacetLocalFindSearchIcon.svg"
-                  className="findCaseIcon"
-                  alt="search"
-                />
-              </div>
-            )}
-          </div>
-          {hasSearch && (
-            <SearchView
-              classes={classes}
-              SearchBox={SearchBox}
-              UploadModal={UploadModal}
-              hidden={!expanded || !showSearch}
-            />
-          )}
-        </CustomExpansionPanelSummary>
-      </>
-
-    );
-  }, [localFindAutocomplete]);
+          CLEAR ALL
+        </Button>
+      </Container>
+    ),
+    []
+  );
 
   /** Note:
-  * Generate Custom facet Section Component
-  * 1. Config local search input for Case
-  * 2. Facet Section Name
-  */
-  const RedirectButton = useCallback(({ onClearAllFilters }) => (
-    <Container className="restBtn">
-      <Button
-        variant="outlined"
-        className="resetButton"
-        href="#/explore"
-        onClick={() => {
-          onClearAllFilters();
-        }}
-        disableRipple
-      >
-        RESET QUERY
-      </Button>
-    </Container>
-  ), []);
+   * Generate Custom facet Section Component
+   * 1. Config local search input for Case
+   * 2. Facet Section Name
+   */
+  const CustomFacetSection = useCallback(
+    ({ section }) => {
+      const { name, expandSection } = section;
+      const { hasSearch = false } = facetSectionVariables[name];
+      const [expanded, setExpanded] = useState(expandSection);
+      const [showSearch, setShowSearch] = useState(true);
+
+      const toggleSearch = (e) => {
+        e.stopPropagation();
+        setShowSearch(!showSearch);
+      };
+      const collapseHandler = () => setExpanded(!expanded);
+
+      return (
+        <>
+          <CustomExpansionPanelSummary
+            onClick={collapseHandler}
+            id={section}
+            className={clsx(
+              {
+                [classes.disableExpansion]: isUnifiedView,
+              },
+              "customExpansionPanelSummaryRoot"
+            )}
+            disabled={isUnifiedView}
+          >
+            <div className="sectionSummaryTextContainer">
+              {name}
+              {hasSearch && (
+                <div className="findCaseButton" onClick={toggleSearch}>
+                  <img
+                    src="https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/svgs/FacetLocalFindSearchIcon.svg"
+                    className="findCaseIcon"
+                    alt="search"
+                  />
+                </div>
+              )}
+            </div>
+            {hasSearch && (
+              <SearchView
+                classes={classes}
+                SearchBox={SearchBox}
+                UploadModal={UploadModal}
+                hidden={!expanded || !showSearch}
+              />
+            )}
+          </CustomExpansionPanelSummary>
+        </>
+      );
+    },
+    [localFindAutocomplete]
+  );
+
+  /** Note:
+   * Generate Custom facet Section Component
+   * 1. Config local search input for Case
+   * 2. Facet Section Name
+   */
+  const RedirectButton = useCallback(
+    ({ onClearAllFilters }) => (
+      <Container className="restBtn">
+        <Button
+          variant="outlined"
+          className="resetButton"
+          href="#/explore"
+          onClick={() => {
+            onClearAllFilters();
+          }}
+          disableRipple
+        >
+          RESET QUERY
+        </Button>
+      </Container>
+    ),
+    []
+  );
 
   /**
-  * Generate Custom facet View Component
-  * 1. Config local search input for Case
-  * 2. Facet Section Name
-  */
-  const CustomFacetView = useCallback(({ facet, facetClasses }) => {
-    const clsName = `${facetClasses}`.replace(/\s+/g, '');
-    return (
-      <>
-        <CustomExpansionPanelSummary
-          expandIcon={(
-            <ExpandMoreIcon
-              classes={{ root: classes.dropDownIconSubSection }}
-              style={{ fontSize: 26 }}
-            />
-          )}
-          id={facet.label}
-          className="customExpansionPanelSummaryRoot"
-        >
-          <div
-            id={facet.label}
-            className={
-              clsx(
-                classes.sectionSummaryText,
-                classes[clsName],
-                'sectionSummaryText',
-              )
+   * Generate Custom facet View Component
+   * 1. Config local search input for Case
+   * 2. Facet Section Name
+   */
+  const CustomFacetView = useCallback(
+    ({ facet, facetClasses, hasSelections, clearFacetSectionValues }) => {
+      console.log("hasSelections", hasSelections);
+      const clsName = `${facetClasses}`.replace(/\s+/g, "");
+      return (
+        <>
+          <CustomExpansionPanelSummary
+            expandIcon={
+              <ExpandMoreIcon
+                classes={{ root: classes.dropDownIconSubSection }}
+                style={{ fontSize: 26 }}
+              />
             }
+            id={facet.label}
+            className="customExpansionPanelSummaryRoot"
           >
-            {facet.label}
-          </div>
-        </CustomExpansionPanelSummary>
-      </>
-    );
-  }, []);
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                id={facet.label}
+                className={clsx(
+                  classes.sectionSummaryText,
+                  classes[clsName],
+                  "sectionSummaryText"
+                )}
+              >
+                {facet.label}
+              </div>
+
+              {hasSelections ? (
+                <IconButton onClick={clearFacetSectionValues}>
+                  <RefreshIcon />
+                </IconButton>
+              ) : null}
+            </div>
+          </CustomExpansionPanelSummary>
+        </>
+      );
+    },
+    []
+  );
   return (
     <FacetFilterThemeProvider>
       <ClearAllFiltersBtn
@@ -287,6 +336,7 @@ const BentoFacetFilter = ({
         facetsConfig={updateFacetConfig}
         CustomFacetSection={CustomFacetSection}
         CustomFacetView={CustomFacetView}
+        enableClearSection
       />
     </FacetFilterThemeProvider>
   );
