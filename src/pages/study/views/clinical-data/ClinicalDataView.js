101e4d929cba2dc9a218ca8b646fae1fa066733d
rana22 <ambar.rana123@gmail.com>
2024-02-01 10:50:40 +0545

    Clinical Data table style fix

--- src/pages/study/views/clinical-data/ClinicalDataView.js
+++ src/pages/study/views/clinical-data/ClinicalDataView.js
@@ -21,11 +21,11 @@ const ClinicalDataView = ({
   return (
     <div className={classes.container}>
       <div>
-        <p className={classes.paragraphOne}>
+        <p className={classes.paragraph}>
           Detailed clinical trial observations from this study can be downloaded from
           any node for which a CSV download option is displayed.
         </p>
-        <p className={classes.paragraphTwo}>
+        <p className={classes.paragraph}>
           The node-specific counts indicate the number of cases represented
           within a node into which data has been propagated versus the number
           of records within such nodes.
