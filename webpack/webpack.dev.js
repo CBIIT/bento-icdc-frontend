ef03726d81c642df0ceb5c3f27380d653a4aa03a
rana22 <ambar.rana123@gmail.com>
2024-05-13 12:43:11 -0400

    webpack dev server configuration

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,17 +3,34 @@ const common = require('./webpack.common');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const paths = require('../config/paths');
+const getClientEnvironment = require('../config/env');
+
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
+const publicUrl = '';
+const env = getClientEnvironment(publicUrl);
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     compress: true,
+    open: true,
     port: 9000,
+    client: {
+      overlay: false,
+    },
   },
   module: {
     rules: [
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]']
+      },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
@@ -31,8 +48,10 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
     new HtmlWebpackPlugin(
       {
+        template: path.resolve(__dirname, "public", "index.html"),
         inject: true,
         template: paths.appHtml,
       },
