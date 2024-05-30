d7f2b715c4bdc7763b21e8be1d57838e297b3fa4
Toyo <toyo.udosen@nih.gov>
2024-05-30 12:16:12 -0500

    add sysInfo page

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -29,6 +29,7 @@ import ShutdownBanner from '../ShutdownBanner/ShutdownBanner';
 import { navBarExclusions } from '../../bento/navigationBarData';
 import styles from './LayoutStyle';
 import CartView from '../../pages/Cart/CartController';
+import SysInfo from '../../pages/sysInfo/sysInfo';
 // import CartView from '../../pages/fileCentricCart/CartController';
 
 const LayoutView = ({
@@ -97,6 +98,7 @@ const LayoutView = ({
               <Route path="/graphql" component={GraphQLView} />
               <Route path="/study/:id" component={StudyDetail} />
               <Route path="/case/:id" component={CaseDetails} />
+              <Route path="/sysinfo" component={SysInfo} />
               {aboutPageRoutes.map(
                 (aboutPageRoute) => <Route path={aboutPageRoute} component={About} />,
               )}
