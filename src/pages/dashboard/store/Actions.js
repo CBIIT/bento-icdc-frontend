11c6150205d0c641d2bcdfa5b9c2667d92eebd59
rana22 <ambar.rana123@gmail.com>
2024-11-10 22:10:56 -0500

    remove store

--- src/pages/dashboard/store/Actions.js
+++
@@ -1,8 +0,0 @@
-export const actionTypes = {
-  ON_SEARCH_INPUT_CHANGE: 'ON_SEARCH_INPUT_CHANGE',
-};
-
-export const onInputSearchQueryChange = value => ({
-  type: actionTypes.ON_SEARCH_INPUT_CHANGE,
-  payload: value,
-});
--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,6 @@ import {
 } from '../bento-core';
 import { jbrowseView } from '../pages/JbrowseDetail/store/jborwse.reducer';
 import stats from '../components/Stats/StatsState';
-import { dashboardReducer } from '../pages/dashboard/store/Reducers';
 
 const { localFind } = LocalFindReducerGenerator();
 const { statusReducer } = sideBarReducerGenerator();
@@ -28,7 +27,6 @@ const reducers = {
   cartReducer,
   jbrowseView,
   stats,
-  dashboardReducer,
 };
 
 const store = configureStore({
