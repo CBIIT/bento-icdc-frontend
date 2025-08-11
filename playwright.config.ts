c246c1286bcda259c12940c2139fd7227ded4504
ty-esi <toyo.udosen@nih.gov>
2025-08-11 17:49:25 -0500

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run dev',
     url: 'http://localhost:7000',
     reuseExistingServer: !process.env.CI,
   },
