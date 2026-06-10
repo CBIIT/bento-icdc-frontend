06f9ae31e8ad84ed02b76957853f97d239da9aed
rana22 <ambar.rana123@gmail.com>
2026-06-10 10:56:02 -0400

    refactor code

--- src/components/CustomIcon/CustomIconView.jsx
+++ src/components/CustomIcon/CustomIconView.jsx
@@ -1,30 +1,3 @@
-// import React from "react";
-// import { withStyles, Icon } from "@material-ui/core";
-
-// const Logo = ({ imgSrc, imgAlt = "Logo alt text", classes, styles }) => (
-//   <Icon>
-//     <img
-//       src={imgSrc}
-//       className={classes.root}
-//       style={{ styles }}
-//       alt={imgAlt}
-//     />
-//   </Icon>
-// );
-
-// const styles = () => ({
-//   root: {
-//     width: "1em",
-//     height: "1em",
-//     position: "relative",
-//     left: "15%",
-//     display: "inline-block",
-//     flexShrink: 0,
-//   },
-// });
-
-// export default withStyles(styles)(Logo);
-
 import React from 'react';
 import { withStyles, Icon } from '@material-ui/core';
 
