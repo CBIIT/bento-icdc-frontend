e1ca0ba324fd5b17f3d034b7d33f3dbd71e0eeb3
rana22 <ambar.rana123@gmail.com>
2026-06-22 15:29:28 -0400

    revert to source code

--- src/components/Layout/LayoutView.tsx
+++ src/components/Layout/LayoutView.tsx
@@ -152,7 +152,6 @@ const LayoutContent = () => {
           #root {
             height: 100%;
             flex: 1;
-            min-width: 0;
           }
           #root > div:nth-child(1) {
             top: var(--site-alert-offset);
